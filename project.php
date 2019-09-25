1b5186e777ee5105fdeb5b64e189dffb13d00e82
MuhummadJIS <mdsohag200077@gmail.com>
2019-09-24 17:41:00 -0700

    Just One Modify

--- project.php
+++ project.php
@@ -1,11 +1,13 @@
 <?php 
     class user{
+        public $greating;
         public $name;
         public $email;
 
-        function __construct($value,$value1){
-            $this->name=$value;
+        function __construct($value,$value1,$value2){
+            $this->greating=$value;
             $this->name=$value1;
+            $this->name=$value2;
         }
     }
 
