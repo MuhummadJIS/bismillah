8d8347fc85fb2b467ed54b9844897b1cec86c618
MuhummadJIS <mdsohag200077@gmail.com>
2019-09-24 17:31:57 -0700

    New File Uplodead

---
+++ project.php
@@ -0,0 +1,14 @@
+<?php 
+    class user{
+        public $name;
+        public $email;
+
+        function __construct($value,$value1){
+            $this->name=$value;
+            $this->name=$value1;
+        }
+    }
+
+
+
+?>
